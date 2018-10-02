1de471837f8715b51322c4193b148ea06fa211c0
Rosi2 <rosa.llanos.13@sansano.usm.cl>
2018-10-02 19:43:56 -0300

    version 1

---
+++ conduit/moleculer.config.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = {
+	namespace: "conduit",
+	//transporter: "TCP",
+	logger: true,
+	logLevel: "info",
+	logFormatter: "short",
+	cacher: {
+		type: "memory",
+		options: {
+			maxParamsLength: 100
+		}
+	},
+	metrics: true
+};
