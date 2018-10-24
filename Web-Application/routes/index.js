deff51f1304a4915fd435a5bd0dcf7e3c26f2815
José Tello <jose.tello.14@sansano.usm.cl>
2018-10-24 15:29:48 -0300

    Commit predemo

--- Web-Application/routes/index.js
+++ Web-Application/routes/index.js
@@ -1,21 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('home', { title: 'FlowerSoft' });
 });
 
-router.get('/registro', function(req, res, next){
-	res.render('registro', {title: 'Registro de Usuarios'});
-});
-
-router.get('/login', function(req, res, next){
-	res.render('login', {title: 'Login'});
-});
-
 router.get('/dashboard', function(req, res, next){
+	var array;
+	request.get('http://localhost:8080/pedidos'), 
+	function optionalCallback(err, response, body){
+		console.log(response);
+		console.log(body);
+		array = body;
+	}
+	console.log(array);
 	res.render('inicio', {title: 'Inicio'});
+});
+
+router.post('/dashboard/crearPedido', function(req, res, next){
+	var detalle = req.body.detalle;
+	var fecha = req.body.fecha;
+	request.post('http://localhost:8080/pedidos').form({"detalle":detalle, "fecha" :fecha}), 
+	function optionalCallback(err, httpResponse, body) {
+  		if (err) {
+    		return console.error('upload failed:', err);
+  		}
+  		console.log('Upload successful!  Server responded with:', body);
+	};
+	res.redirect('/dashboard');
 });
 
 module.exports = router;
--- Web-Application/routes/users.js
+++ Web-Application/routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/registro', function(req, res, next){
+	res.render('registro', {title: 'Registro de Usuarios'});
+});
+
+router.get('/login', function(req, res, next){
+	res.render('login', {title: 'Login'});
+});
+
 module.exports = router;
