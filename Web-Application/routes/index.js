a4132703dffe600190b407f95e3aeef6ef957588
José Tello <jose.tello.14@sansano.usm.cl>
2018-11-03 18:14:35 -0300

    Agregado CRUD de pedidos

--- Web-Application/routes/index.js
+++ Web-Application/routes/index.js
@@ -8,16 +8,47 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/dashboard', function(req, res, next){
-	var array;
-	request.get('http://localhost:8080/pedidos'), 
-	function optionalCallback(err, response, body){
-		console.log(response);
-		console.log(body);
-		array = body;
-	}
-	console.log(array);
-	res.render('inicio', {title: 'Inicio'});
+	request('http://localhost:8080/pedidos/', function(error, response, body) {
+		var pedidos = JSON.parse(body);
+        res.render('inicio', {title: 'Inicio', pedidos: pedidos.data});
+    });
+	//res.render('inicio', {title: 'Inicio', pedidos: array.data});
 });
+
+router.get('/dashboard/pedido/delete/:id', function(req, res, next){
+	var id = req.params.id;
+	request.delete('http://localhost:8080/pedidos/'+id, 
+		function optionalCallback(err, httpResponse, body){
+			if (err) {
+    		return console.error('Delete failed:', err);
+  			}
+  			console.log('Delete successful!  Server responded with:', body);	
+		}
+	);
+	res.redirect('/dashboard');
+}); //Funcionando
+
+router.get('/dashboard/pedido/edit/:id', function(req, res, next){
+	var id = req.params.id;
+	request('http://localhost:8080/pedidos/'+id, function(error, response, body) {
+		var pedido = JSON.parse(body);
+        res.render('edit_pedido', {pedido: pedido.data});
+    });
+});
+
+router.post('/dashboard/pedido/update', function(req, res, next){
+	var id = req.body.id;
+	var detalle = req.body.detalle;
+	var fecha = req.body.fecha;
+	request.put('http://localhost:8080/pedidos/'+id).form({"detalle":detalle, "fecha" :fecha}), 
+	function optionalCallback(err, httpResponse, body) {
+  		if (err) {
+    		return console.error('upload failed:', err);
+  		}
+  		console.log('Upload successful!  Server responded with:', body);
+	};
+	res.redirect('/dashboard');
+})
 
 router.post('/dashboard/crearPedido', function(req, res, next){
 	var detalle = req.body.detalle;
