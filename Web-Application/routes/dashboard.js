d74526d8dd5ed44b2706bc3536afced5cda45477
José Tello <jose.tello.14@sansano.usm.cl>
2018-11-06 03:34:53 -0300

    Se hicieron cambios en el frontend, se ordenaron las rutas de la web-app

---
+++ Web-Application/routes/dashboard.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	//Rosa debe poblar esta vista con los productos
+  res.render('dashboard', { title: 'Dashboard' });
+});
+
+
+router.get('/pedidos/', function(req, res, next){
+	request('http://localhost:8080/pedidos/', function(error, response, body) {
+		var pedidos = JSON.parse(body);
+        res.render('pedidos', {title: 'Inicio', pedidos: pedidos.data});
+    });
+	//res.render('inicio', {title: 'Inicio', pedidos: array.data});
+});
+
+router.get('/pedido/delete/:id', function(req, res, next){
+	var id = req.params.id;
+	request.delete('http://localhost:8080/pedidos/'+id, 
+		function optionalCallback(err, httpResponse, body){
+			if (err) {
+    		return console.error('Delete failed:', err);
+  			}
+  			console.log('Delete successful!  Server responded with:', body);	
+		}
+	);
+	res.redirect('/dashboard/pedidos');
+}); //Funcionando
+
+router.get('/pedido/edit/:id', function(req, res, next){
+	var id = req.params.id;
+	request('http://localhost:8080/pedidos/'+id, function(error, response, body) {
+		var pedido = JSON.parse(body);
+        res.render('edit_pedido', {pedido: pedido.data});
+    });
+});
+
+router.post('/pedido/update', function(req, res, next){
+	var id = req.body.id;
+	var detalle = req.body.detalle;
+	var fecha = req.body.fecha;
+	request.put('http://localhost:8080/pedidos/'+id).form({"detalle":detalle, "fecha" :fecha}), 
+	function optionalCallback(err, httpResponse, body) {
+  		if (err) {
+    		return console.error('upload failed:', err);
+  		}
+  		console.log('Upload successful!  Server responded with:', body);
+	};
+	res.redirect('/dashboard/pedidos');
+})
+
+router.get('/crearPedido', function(req, res, next) {
+	//Rosa debe poblar esta vista con los productos
+  res.render('crear_pedido', { title: 'Crear Pedido' });
+});
+
+router.post('/crearPedido', function(req, res, next){
+	var detalle = req.body.detalle;
+	var fecha = req.body.fecha;
+	request.post('http://localhost:8080/pedidos/').form({"detalle":detalle, "fecha" :fecha}), 
+	function optionalCallback(err, httpResponse, body) {
+  		if (err) {
+    		return console.error('upload failed:', err);
+  		}
+  		console.log('Upload successful!  Server responded with:', body);
+	};
+	res.redirect('/dashboard/pedidos');
+});
+
+
+module.exports = router;
--- Web-Application/routes/index.js
+++ Web-Application/routes/index.js
@@ -4,63 +4,17 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'FlowerSoft' });
+  res.render('index', { title: 'FlowerSoft' });
 });
 
-router.get('/dashboard', function(req, res, next){
-	request('http://localhost:8080/pedidos/', function(error, response, body) {
-		var pedidos = JSON.parse(body);
-        res.render('inicio', {title: 'Inicio', pedidos: pedidos.data});
-    });
-	//res.render('inicio', {title: 'Inicio', pedidos: array.data});
+router.get('/registro', function(req, res, next) {
+  res.render('registro', { title: 'FlowerSoft' });
 });
 
-router.get('/dashboard/pedido/delete/:id', function(req, res, next){
-	var id = req.params.id;
-	request.delete('http://localhost:8080/pedidos/'+id, 
-		function optionalCallback(err, httpResponse, body){
-			if (err) {
-    		return console.error('Delete failed:', err);
-  			}
-  			console.log('Delete successful!  Server responded with:', body);	
-		}
-	);
-	res.redirect('/dashboard');
-}); //Funcionando
-
-router.get('/dashboard/pedido/edit/:id', function(req, res, next){
-	var id = req.params.id;
-	request('http://localhost:8080/pedidos/'+id, function(error, response, body) {
-		var pedido = JSON.parse(body);
-        res.render('edit_pedido', {pedido: pedido.data});
-    });
+router.post('/login', function(req, res, next){
+	//Max debe hacer la conexion entre api-gateway y su servicio, para luego redireccionar al dashboard
 });
 
-router.post('/dashboard/pedido/update', function(req, res, next){
-	var id = req.body.id;
-	var detalle = req.body.detalle;
-	var fecha = req.body.fecha;
-	request.put('http://localhost:8080/pedidos/'+id).form({"detalle":detalle, "fecha" :fecha}), 
-	function optionalCallback(err, httpResponse, body) {
-  		if (err) {
-    		return console.error('upload failed:', err);
-  		}
-  		console.log('Upload successful!  Server responded with:', body);
-	};
-	res.redirect('/dashboard');
-})
 
-router.post('/dashboard/crearPedido', function(req, res, next){
-	var detalle = req.body.detalle;
-	var fecha = req.body.fecha;
-	request.post('http://localhost:8080/pedidos').form({"detalle":detalle, "fecha" :fecha}), 
-	function optionalCallback(err, httpResponse, body) {
-  		if (err) {
-    		return console.error('upload failed:', err);
-  		}
-  		console.log('Upload successful!  Server responded with:', body);
-	};
-	res.redirect('/dashboard');
-});
 
 module.exports = router;
